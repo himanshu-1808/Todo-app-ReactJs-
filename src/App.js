d11d3c2763342d2f84c870e1e1e337833c498030
Himanshu Panchal <himanshu1112.cse19@chitkara.edu.in>
2022-02-11 21:03:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+<br/>
+<input type="text" placeholder="Add items"/>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
